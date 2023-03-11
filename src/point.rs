9231e2aee460d603e39765f3409764ecf3b0845c
Fletterio <fj.letterio@gmail.com>
2023-03-11 18:36:36 -0300

    Implemented initialization of an empty Simplex Tree (all points as 0-simplices)

--- src/point.rs
+++ src/point.rs
@@ -1,7 +1,15 @@
 use std::cmp::PartialOrd;
 
-#[derive(PartialEq, PartialOrd)]
+#[derive(PartialEq, PartialOrd, Debug)]
 //should I use const generics?
 pub struct Point {
-    coordinates: Vec<f64>,
+    pub coordinates: Vec<f64>,
+}
+
+impl Eq for Point {}
+
+impl Ord for Point {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        self.partial_cmp(other).unwrap()
+    }
 }
--- src/simplex_tree.rs
+++ src/simplex_tree.rs
@@ -1,20 +1,16 @@
 use crate::point::Point;
 use hashbrown::HashMap;
-use std::collections::BTreeSet;
+use std::{cell::RefCell, collections::BTreeSet, rc::Rc};
 
-#[derive(PartialEq, Eq, Hash)]
-struct Node<'a> {
-    value: u64,
-    children: NodeSet<'a>,
+#[derive(PartialEq, Eq, PartialOrd, Debug)]
+struct Node {
+    value: usize,
+    //depth: u64,  //optional for now: use will depend on whether not having it could cause mistakes
+    children: BTreeSet<Rc<RefCell<Node>>>,
+    parent: Option<Rc<RefCell<Node>>>,
 }
 
-#[derive(PartialEq, Eq, Hash)]
-struct NodeSet<'a> {
-    values: BTreeSet<Node<'a>>,
-    parent: &'a Node<'a>,
-}
-
-type LevelList<'a> = Vec<&'a NodeSet<'a>>;
+type LevelList<'a> = Vec<&'a Node>;
 
 //holds a map from levels to a map from labels to corresponding levellists
 struct LevelListMap<'a> {
@@ -22,8 +18,12 @@ struct LevelListMap<'a> {
 }
 
 impl<'a> LevelListMap<'a> {
-    //returns a map from depth levels j to all LevelLists L(j, label) (or None)
-    fn levels(&mut self, label: u64) -> Option<&mut HashMap<u64, LevelList<'a>>> {
+    //returns a map from depth levels j to all LevelLists(j, label)
+    fn levels(&self, label: u64) -> Option<&HashMap<u64, LevelList<'a>>> {
+        self.matrix.get(&label)
+    }
+    //returns a mutable map from depth levels j to all LevelLists L(j, label) (or None)
+    fn mut_levels(&mut self, label: u64) -> Option<&mut HashMap<u64, LevelList<'a>>> {
         self.matrix.get_mut(&label)
     }
     //returns a reference to the LevelList L(level, label) (or None)
@@ -34,37 +34,50 @@ impl<'a> LevelListMap<'a> {
             None
         }
     }
-
-    //we insert a given NodeSet in the LevelList L(level, label)
-    fn insert_node_set(&mut self, node_set: &'a NodeSet<'a>, label: u64, level: u64) -> bool {
-        let mut levels: &mut HashMap<u64, LevelList<'a>> = &mut HashMap::new();
-        if let Some(lvl) = self.levels(label) {
-            levels = lvl;
-            if let Some(level_list) = levels.get_mut(&level) {
-                if level_list.contains(&node_set) {
-                    false
-                } else {
-                    level_list.push(node_set);
-                    true
-                }
-            } else {
-                let mut level_list = LevelList::with_capacity(1);
-                level_list.push(node_set);
-                levels.insert(level, level_list);
-                true
-            }
+    //returns a mutable reference to the LevelList L(level, label) (or None)
+    fn mut_level_list(&mut self, label: u64, level: u64) -> Option<&mut LevelList<'a>> {
+        if let Some(levels) = self.matrix.get_mut(&label) {
+            levels.get_mut(&level)
         } else {
-            self.matrix.insert(label, *levels);
-            let mut level_list = LevelList::with_capacity(1);
-            level_list.push(node_set);
-            levels.insert(level, level_list);
-            true
+            None
         }
     }
 
+    //we insert a given NodeSet in the LevelList L(level, label)
+    fn insert_node_set(&mut self, node: &'a Node, label: u64, level: u64) -> bool {
+        let at_least_one_level: bool;
+        if let Some(levels) = self.mut_levels(label) {
+            at_least_one_level = true;
+            if let Some(level_list) = levels.get_mut(&level) {
+                if level_list.contains(&node) {
+                    return false;
+                } else {
+                    level_list.push(node);
+                    return true;
+                }
+            } else {
+                let mut level_list = LevelList::with_capacity(1);
+                level_list.push(node);
+                levels.insert(level, level_list);
+                return true;
+            }
+        } else {
+            at_least_one_level = false;
+        }
+        if !at_least_one_level {
+            let mut levels: HashMap<u64, LevelList<'a>> = HashMap::new();
+            let mut level_list = LevelList::with_capacity(1);
+            level_list.push(node);
+            levels.insert(level, level_list);
+            self.matrix.insert(label, levels);
+            return true;
+        }
+        panic!("Code should never reach here");
+    }
+
     //removes a certain NodeSet (possibly) stored in L(level, label)
-    fn remove_node_set(&mut self, node_set: &'a NodeSet<'a>, label: u64, level: u64) -> bool {
-        if let Some(level_list) = self.level_list(label, level) {
+    fn remove_node_set(&mut self, node_set: &'a Node, label: u64, level: u64) -> bool {
+        if let Some(level_list) = self.mut_level_list(label, level) {
             if let Some(pos) = level_list.iter().position(|x| *x == node_set) {
                 level_list.remove(pos);
                 true
@@ -77,8 +90,65 @@ impl<'a> LevelListMap<'a> {
     }
 }
 
-/*pub struct SimplexTree {
+#[derive(Debug)]
+pub struct SimplexTree {
+    pub points: Vec<Point>,
     top_nodes: Vec<Node>, //set the parent node to ROOT
-}*/
+}
 
 //constructors
+//the SimplexTree originally only holds all points as 0-simplices
+impl SimplexTree {
+    fn new(p: Vec<Point>) -> Self {
+        let mut p = p;
+        p.sort();
+        let nodes: Vec<Node> = p
+            .iter()
+            .enumerate()
+            .map(|(i, _x)| Node {
+                value: i,
+                children: BTreeSet::new(),
+                parent: None,
+            })
+            .collect();
+        SimplexTree {
+            points: p,
+            top_nodes: nodes,
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_creation() {
+        let points = vec![
+            Point {
+                coordinates: vec![1.0, 1.0, 1.0],
+            },
+            Point {
+                coordinates: vec![0.0, 5.0, 10.0],
+            },
+            Point {
+                coordinates: vec![2.0, -1.0, 7.0],
+            },
+        ];
+        let tree = SimplexTree::new(points);
+        println!("{:?}", tree);
+        assert_eq!(
+            tree.points,
+            vec![
+                Point {
+                    coordinates: vec![0.0, 5.0, 10.0],
+                },
+                Point {
+                    coordinates: vec![1.0, 1.0, 1.0],
+                },
+                Point {
+                    coordinates: vec![2.0, -1.0, 7.0],
+                }
+            ]
+        );
+    }
+}
